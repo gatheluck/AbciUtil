57ca3393b4775e4bc8edccee924aea9f3ff0ce0b
gatheluck <gatheluck+tech@gmail.com>
2020-07-30 06:05:03 +0900

    add shell option

--- apps/run.py
+++ apps/run.py
@@ -15,5 +15,5 @@ if __name__ == '__main__':
     for script in scripts:
         if script.endswith('.sh'):
             cmd = 'qsub -g gca50149 {target_script}'.format(target_script=script)
-            subprocess.call(cmd)
+            subprocess.call(cmd, shell=True)
             time.sleep(3)  # to prevent confliction of hydra save dirs.
